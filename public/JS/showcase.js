55e2b3bb44e33dce50164d64452750249e2fa9a3
Status100 <bluemagicuk@yahoo.co.uk>
2019-11-12 13:07:30 +0000

    add fade in function on scroll

--- public/JS/showcase.js
+++ public/JS/showcase.js
@@ -12,3 +12,22 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+$(function () { // $(document).ready shorthand
+    $('.monster').fadeIn('slow');
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        console.log('moose');
+        $('.reveal').each(function (i) {
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_object) {
+                $(this).animate({
+                    'opacity': '1'
+                }, 1500);
+            }
+        });
+    });
+});
