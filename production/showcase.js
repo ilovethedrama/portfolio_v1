e55557b482514e76a8a9e7f8a57d96cb2ff70ad7
Status100 <bluemagicuk@yahoo.co.uk>
2019-11-12 13:43:38 +0000

    updated production versions of css and js

---
+++ production/showcase.js
@@ -0,0 +1,33 @@
+var acc = document.getElementsByClassName("img__tile--primary");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+$(function () { // $(document).ready shorthand
+    $('.monster').fadeIn('slow');
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        console.log('moose');
+        $('.reveal').each(function (i) {
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_object) {
+                $(this).animate({
+                    'opacity': '1'
+                }, 1500);
+            }
+        });
+    });
+});
