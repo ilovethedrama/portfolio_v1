87233d303f9b7fddd4f3589451026b5e2995e0d0
Status100 <bluemagicuk@yahoo.co.uk>
2019-11-13 03:41:06 +0000

    add fadein logic for effect on title

--- public/JS/showcase.js
+++ public/JS/showcase.js
@@ -30,4 +30,28 @@ $(document).ready(function () {
             }
         });
     });
+
+    var count = 0;
+    var maxDelay = 5500;
+    var minSpeed = 1500;
+    var maxSpeed = 1000;
+    var fadeTo = 1;
+
+    $('.peepThis').each(function () {
+        console.log('weeeeoooo');
+        var delay = Math.ceil(Math.random() * maxDelay);
+        var speed = maxSpeed + Math.ceil(Math.random() * (minSpeed - maxSpeed));
+        count++;
+        $(this).delay(delay).fadeTo(speed, fadeTo, function () {
+            count--;
+            if (count == 0) {
+                onFinish();
+            }
+        });
+    });
+
+    function onFinish() {}
+
+
+
 });
