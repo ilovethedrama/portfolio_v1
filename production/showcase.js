e1523cf36062261fc0d1f3c828653d11e62b38a1
Status100 <bluemagicuk@yahoo.co.uk>
2019-11-15 13:13:10 +0000

    updated js file

--- production/showcase.js
+++ production/showcase.js
@@ -30,4 +30,43 @@ $(document).ready(function () {
             }
         });
     });
+
+    //     const word1 = document.getElementById('introWord1');
+    //     const word2 = document.getElementById('introWord2');
+    //     const word3 = document.getElementById('introWord3');
+    //     const word4 = document.getElementById('introWord4');
+    //     const word5 = document.getElementById('introWord5');
+    //     const word6 = document.getElementById('introWord6');
+
+    //     var wordList = [word1, word2, word3, word4, word5, word6];
+
+    //  wordList.forEach(function (word) {
+    //      setInterval
+    //         word.style.opacity = 1;
+    //     });
+
+
+    var count = 0;
+    var maxDelay = 5500;
+    var minSpeed = 1500;
+    var maxSpeed = 1000;
+    var fadeTo = 1;
+
+    $('.peepThis').each(function () {
+        console.log('weeeeoooo');
+        var delay = Math.ceil(Math.random() * maxDelay);
+        var speed = maxSpeed + Math.ceil(Math.random() * (minSpeed - maxSpeed));
+        count++;
+        $(this).delay(delay).fadeTo(speed, fadeTo, function () {
+            count--;
+            if (count == 0) {
+                onFinish();
+            }
+        });
+    });
+
+    function onFinish() {}
+
+
+
 });
